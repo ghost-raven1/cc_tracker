0064936124df922d844110a9beff57405a28147d
alexdan93 <alek9309@mail.ru>
2022-06-22 15:12:43 +0700

    [api-fix] fix api call

--- lib/CCList.dart
+++ lib/CCList.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cc_tracker/CCData.dart';
 import 'package:http/http.dart' as http;
@@ -31,17 +32,29 @@ class CCListState extends State<CCList>{
     );
   }
 
+  Map<String, String> get headers => {
+    'X-CMC_PRO_API_KEY': '',
+  };
+
   _loadCC() async{
-    final response = await http.get(Uri.parse(''));
+    final response = await http.get(
+      Uri.parse('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'),
+      headers: headers,
+    );
     if(response.statusCode == 200) {
       var responseData= json.decode(response.body);
       var allData = responseData['data'] as List<dynamic>;
       List<CCData> ccDataList = [];
 
       for (var val in allData) {
-        var record = CCData(name: val['name'],
-            symbol: val['symbol'], rank: val['rank'], price: val['priceUSD']);
+        var record = CCData(
+            name: val['name'],
+            symbol: val['symbol'],
+            rank: val['cmc_rank'],
+            price: val['quote']['USD']['price']
+        );
         ccDataList.add(record);
+        data = ccDataList;
       }
     }
 }
@@ -51,7 +64,7 @@ class CCListState extends State<CCList>{
       title: Text(f.symbol),
       subtitle: Text(f.name),
       leading: CircleAvatar(child: Text(f.rank.toString())),
-      trailing: Text('\$${f.price.toString()}'),
+      trailing: Text('\$${f.price.toStringAsFixed(4)}'),
     )).toList();
   }
 }
